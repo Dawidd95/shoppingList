33b81fea304eeda2fa32017f9f563aa3e90acb21
Dawidd95 <32812931+Dawidd95@users.noreply.github.com>
2018-12-30 16:03:49 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 class App extends React.Component {
    state = {
+      selected: 'all',
       inputElement: '',
       listElements: [],
       elementId: 0,
@@ -20,7 +21,7 @@ class App extends React.Component {
       shoppingList = this.state.listElements;
 
       if(this.state.inputElement !== '') {
-         shoppingList.push({id: this.state.elementId, value: this.state.inputElement});
+         shoppingList.push({id: this.state.elementId, value: this.state.inputElement, type: 'favorite'});
 
          this.setState(prevState => ({
             listElements: shoppingList,
@@ -45,33 +46,99 @@ class App extends React.Component {
       })
    }
 
+   handleFilterClick = (type) => {
+      if(type === 'Pokaż wszystkie') {
+         this.setState({
+            selected: 'all'
+         })
+      } else {
+         this.setState({
+            selected: 'favorites'
+         })
+      }
+   }
+
+   filterBooks = () => {
+      if(this.state.selected === 'all') {
+         return(
+            <div>
+               <Title 
+                  content='My Shopping List'
+               />
+               <AllBooks 
+                  inputElement={this.state.inputElement}
+                  listElements={this.state.listElements}
+                  elementId={this.state.elemetId}
+                  errInformation={this.state.errInformation}
+                  changeElementNameInput={this.handleElementNameInputChange}
+                  clickAddToShoppingList={this.handleAddToShoppingList}              
+                  removeFromListClick={this.handleRemoveFromListClick}
+               />
+            </div>          
+         )
+      } else {
+         return(
+            <div>
+               <Title 
+                  content='Favorites'
+               />
+            </div>   
+         )
+      }
+   } 
+
    render() { 
       return (  
          <div className='app'>
-            <Title />
-            <AddForm 
-               changeElementNameInput={this.handleElementNameInputChange}
-               clickAddToShoppingList={this.handleAddToShoppingList}
-               inputElement={this.state.inputElement}
+            <FilterButtons 
+               filterClick={this.handleFilterClick}
             />
-            <ListAmount 
-               listElements={this.state.listElements}
-            />
-            {this.state.errInformation && 
-               <p className="app__error">Nie możesz dodać do listy pustej pozycji</p>
-            }
-            <ListElements 
-               listElements={this.state.listElements}
-               elementId={this.state.elemetId}
-               removeFromListClick={this.handleRemoveFromListClick}
-            />
+            {this.filterBooks()}           
          </div>
       );
    }
 }
 
-const Title = () => (
-   <header className='app__header'>My Shopping List</header>
+const FilterButtons = ({filterClick}) => (
+   <div className='app__filter'>
+      <Button 
+         content='Pokaż wszystkie'
+         filterClick={filterClick}
+      />
+      <Button 
+         content='Ulubione'
+         filterClick={filterClick}
+      />
+   </div>
+)
+
+const Button = ({content, filterClick}) => (
+   <button className='filter__button' onClick={() => filterClick(content)}>{content}</button>
+)
+
+const Title = ({content}) => (
+   <header className='app__header'>{content}</header>
+)
+
+const AllBooks = ({inputElement, listElements, elementId, errInformation, changeElementNameInput, clickAddToShoppingList, removeFromListClick}) => (
+   <div>
+      <AddForm 
+         changeElementNameInput={changeElementNameInput}
+         clickAddToShoppingList={clickAddToShoppingList}
+         inputElement={inputElement}
+      />
+      <ListAmount 
+         listElements={listElements}
+      />
+      {errInformation && 
+         <p className="app__error">Nie możesz dodać do listy pustej pozycji</p>
+      }
+      <ListElements 
+         listElements={listElements}
+         elementId={elementId}
+         removeFromListClick={removeFromListClick}
+      />
+   </div>
 )
  
 const AddForm = ({changeElementNameInput, clickAddToShoppingList, inputElement}) => (
@@ -87,7 +154,7 @@ const ListAmount = ({listElements}) => (
    <p className='app__amount'>Ilość pozycji na liście: {listElements.length}</p>
 )
 
-const ListElements = ({listElements, elementId, removeFromListClick}) => (
+const ListElements = ({listElements, removeFromListClick}) => (
    <ul className='app__list'>
       {listElements.map(currentElement => <Element 
          key={currentElement.id}
@@ -101,6 +168,7 @@ const Element = (props) => (
    <div className='element'>
       <li className='list__element'>{props.value}</li>
       <img src="del.svg" alt="usun" onClick={() => props.removeFromListClick(props.id)}/>
+      <p className='element__type'>{props.type}</p>
    </div>
 )
 
